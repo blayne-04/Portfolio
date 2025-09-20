c7c574ff4f9398f0b23e94a774af1563f90512b4
blayne-04 <befuller2004@gmail.com>
2025-09-20 14:58:53 -0700

    Added Header, Footer, Background Parallax, Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [tailwind()],
 });
+
